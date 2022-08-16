34731af42daddd1e62cbe12f26b9868a53f0a691
Maksim Dryutov <max_005_27@inbox.lv>
2022-08-17 01:23:04 +0300

    init

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from "react";
+import { RepoCard } from "../components/RepoCard";
+import { useDebounce } from "../hooks/debounce";
+import {
+  useLazyGetUserReposQuery,
+  useSearchUsersQuery,
+} from "../store/github/github.api";
+
+export function HomePage() {
+  const [search, setSearch] = useState("");
+  const [dropdown, setDropdown] = useState(false);
+  const debounced = useDebounce(search);
+  const { isLoading, isError, data } = useSearchUsersQuery(debounced, {
+    skip: debounced.length < 3,
+    refetchOnFocus: false,
+  });
+  const [fetchRepos, { isLoading: areReposLoading, data: repos }] =
+    useLazyGetUserReposQuery();
+
+  useEffect(() => {
+    setDropdown(debounced.length > 3 && data?.length! > 0);
+  }, [data, debounced]);
+
+  const clickHandler = (userName: string) => {
+    fetchRepos(userName);
+    setDropdown(false);
+  };
+
+  return (
+    <div className="flex justify-center pt-10 mx-auto h-screen w-screen">
+      {isError && (
+        <p className="text-center text-red-700">
+          Oops...Something went wrong...
+        </p>
+      )}
+
+      <div className="relative w-[650px]">
+        <input
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          type="text"
+          className="border py-2 px-4 w-full h-[42px] mb-2"
+          placeholder="Search for Github username..."
+        />
+
+        {dropdown && (
+          <ul className="list-none absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white overflow-y-scroll">
+            {isLoading && <p className="text-center">Loading...</p>}
+            {data?.map((user) => (
+              <li
+                key={user.id}
+                onClick={() => clickHandler(user.login)}
+                className="py-2 px-4 hover:bg-gray-500 hover:text-white transition-colors cursor-pointer"
+              >
+                {user.login}
+              </li>
+            ))}
+          </ul>
+        )}
+        <div className="container">
+          {areReposLoading && (
+            <p className="text-center">Repos are loading...</p>
+          )}
+          {repos?.map((repo) => (
+            <RepoCard repo={repo} key={repo.id} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
